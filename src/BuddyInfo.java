619b29cf3ef6c17b2d6a05df237c3d203d26f11a
matthewcrooke3 <matthewcrooke3@me4324-14.labs.sce.carleton.ca>
2014-09-18 17:07:43 -0400

    Commit the right file

---
+++ src/BuddyInfo.java
@@ -0,0 +1,36 @@
+
+public class BuddyInfo {
+		String name;
+		int phonenumber;
+		String address;
+	public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getPhonenumber() {
+			return phonenumber;
+		}
+		public void setPhonenumber(int phonenumber) {
+			this.phonenumber = phonenumber;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BuddyInfo x = new BuddyInfo();
+		String buddy = "Homer";
+		x.setName(buddy);
+		System.out.println("Hello" + " " + x.getName());
+	
+	
+	
+	}
+
+}
